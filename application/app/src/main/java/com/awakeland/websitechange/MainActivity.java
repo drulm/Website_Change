bfb4dc5be64ecf585ad421a829d05eedf23facbe
Darrell Ulm <darrellulm@gmail.com>
2017-01-17 15:37:20 -0500

    Async-Method
    Trying Async method test.

--- application/app/src/main/java/com/awakeland/websitechange/MainActivity.java
+++ application/app/src/main/java/com/awakeland/websitechange/MainActivity.java
@@ -1,6 +1,9 @@
 package com.awakeland.websitechange;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -18,12 +22,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button button;
     private volatile int networkReturnStatus;
+    private EditText statusCode;
 
     @Override
     public void onClick(View v) {
         int status;
 
-        Log.v("WebsiteChange", "-----Start Ping-----");
+        /*Log.v("WebsiteChange", "-----Start Ping-----");
         status = checkSite("http://google.com");
         Log.v("WebsiteChange", "Y: Status Returned:" + status);
         status = checkSite("192.168.0.3");
@@ -34,9 +39,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.v("WebsiteChange", "No: Status Returned:" + status);
         status = checkSite("http://google.com");
         Log.v("WebsiteChange", "Yes: Status Returned:" + status);
-        Log.v("WebsiteChange", "-----End Ping-----");
+        Log.v("WebsiteChange", "-----End Ping-----");*/
 
-        Snackbar.make(v, "Network status code returned: " + status, Snackbar.LENGTH_LONG)
+        WebPageTask task = new WebPageTask();
+        task.execute("http://google.com" );
+        //task.execute("192.168.0.3" );
+        //task.execute("http://yahoo.com" );
+        //task.execute("http://nosite.nosite" );
+
+
+        Snackbar.make(v, "Network status code", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
     }
 
@@ -57,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         button = (Button) findViewById(R.id.check_host);
-
         button.setOnClickListener(this);
     }
 
@@ -104,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 int responseCode;
 
-                //super.run();
+                super.run();
                 try {
                     URL url = new URL(theURL);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -127,5 +138,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return networkReturnStatus;
     }
 
+
+    private class WebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... theURL) {
+            int responseCode;
+            String responseReturnString = "";
+
+            Log.i("WebsiteChange", "2:In WebPageTask " + theURL[0]);
+            try {
+                URL url = new URL(theURL[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("HEAD");
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                networkReturnStatus = responseCode;
+                Log.i("WebsiteChange", "2:getResponseCode(): " + responseCode);
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    responseReturnString = "Connection" + responseCode;
+                    //Log.i("2:WebsiteChange", "2:Connected!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                responseReturnString = "No Connection";
+                //Log.i("2:WebsiteChange", "2:No Connection");
+            }
+            return responseReturnString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.i("2:WebsiteChange", result);
+            myHandler.sendEmptyMessage(0);
+        }
+    }
+
+    Handler myHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    // calling to this function from other pleaces
+                    // The notice call method of doing things
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
 }
+
 
