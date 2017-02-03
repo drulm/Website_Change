4c82689c46ce39306f71df87100dfadf69f5a763
Darrell Ulm <darrellulm@gmail.com>
2017-02-03 16:11:47 -0500

    Small update to load save to file
    More to do on this.

--- application/app/src/main/java/com/awakeland/websitechange/SiteEdit.java
+++ application/app/src/main/java/com/awakeland/websitechange/SiteEdit.java
@@ -1,18 +1,22 @@
 package com.awakeland.websitechange;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.webkit.URLUtil;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.Arrays;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.ObjectInputStream;
 
 /**
  *
@@ -21,9 +25,12 @@ public class SiteEdit extends ListActivity implements View.OnClickListener {
 
     private Button addSite;
     EditText et;
-    FileInputStream fileStream;
+    FileInputStream inputStream;
+    FileOutputStream outputStream;
+    String SITEFILE = "SiteEditFile";
     String listSites[] = {"https://google.com", "https://news.google.com", "https://bing.com"};
-
+    String siteListJoined;
+    List values = new ArrayList();
 
     /**
      *
@@ -37,25 +44,26 @@ public class SiteEdit extends ListActivity implements View.OnClickListener {
         addSite = (Button) findViewById(R.id.buttonSiteAdd);
         addSite.setOnClickListener(this);
 
-        // Test a file write.
+        et = (EditText) findViewById(R.id.siteAddress);
+
+        //for (int i = 0; i < listSites.length; i++)
+
         try {
-            fileStream = openFileInput("ws_sitelist");
-            ObjectInputStream ois = new ObjectInputStream(fileStream);
-            ArrayList<Object> listSitesObjects = (ArrayList<Object>) ois.readObject();
-            ois.close();
+            inputStream  = openFileInput(SITEFILE);
+            inputStream.read(siteListJoined.getBytes());
+            inputStream.close();
+            List<String> values = new ArrayList<String>(Arrays.asList(siteListJoined.split("|||")));
+            Log.i("WebsiteChange", "SiteEdit: SITEFILE: " + Arrays.toString(values.toArray()));
         } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        et = (EditText) findViewById(R.id.siteAddress);
-        List values = new ArrayList();
-        for (int i = 0; i < listSites.length; i++) {
-            values.add(listSites[i]);
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
+
+
+
     }
 
 
@@ -69,13 +77,27 @@ public class SiteEdit extends ListActivity implements View.OnClickListener {
 
         switch (view.getId()) {
             case R.id.buttonSiteAdd:
-                List myList = new ArrayList();
+                //List myList = new ArrayList();
                 // Only add if string is not empty.
                 site = et.getText().toString();
                 if (! site.isEmpty() &&  URLUtil.isValidUrl(site)) {
-                    myList.add(site);
+                    values.add(site);
                     adapter.add(site);
                     et.setText("");
+                    siteListJoined = TextUtils.join("|||", values);
+
+                    try {
+                        /* fileStream = openFileInput(SITEFILE);
+                        ObjectInputStream ois = new ObjectInputStream(fileStream);
+                        ArrayList<Object> listSitesObjects = (ArrayList<Object>) ois.readObject();
+                        ois.close();*/
+                        Log.i("WebsiteChange", "SiteEdit: SITEFILE: " + SITEFILE);
+                        outputStream  = openFileOutput(SITEFILE, Context.MODE_PRIVATE);
+                        outputStream.write(siteListJoined.getBytes());
+                        outputStream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 else {
                     // Create snackbar message.
